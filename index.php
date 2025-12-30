11033afb5fa169e41183a35dec8484505e18819f
alikamal234-gif <alikamal0675@gmail.com>
2025-12-30 16:36:09 +0100

    UCCV-29 :  la structure de dashboard admin html tailwind

--- index.php
+++ index.php
@@ -394,6 +394,7 @@
                 `;
             } else if (type == 'department') {
                 modalForm.innerHTML = `
+                
                     <div class="mb-4"><label class="block text-gray-300 text-sm font-medium mb-2">Nom de Department</label><input class="w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-lg text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500" type="email" value=""></div>
                     <div class="mb-4"><label class="block text-gray-300 text-sm font-medium mb-2">Location</label><input class="w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-lg text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500" type="email" value=""></div>
                 `;
